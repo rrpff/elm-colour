1d9b54707ee7e539e03c6dbd32518ffe0be44d2e
Richard Foster <richard@rpf.me>
2021-08-19 09:10:04 +0100

    Support converting hex to hsl

--- src/Colour.elm
+++ src/Colour.elm
@@ -1,6 +1,10 @@
-module Colour exposing (hexToRgb)
+module Colour exposing (hexToRgbString, hexToHslString)
 import Regex
 import ParseInt
+
+type alias Hex = String
+type alias Rgb = { r : Float, g : Float, b : Float }
+type alias Hsl = { h : Float, s : Float, l : Float }
 
 hexRegex : Regex.Regex
 hexRegex =
@@ -9,38 +13,113 @@ hexRegex =
       { caseInsensitive = True, multiline = False }
       "^#?([a-f\\d]{2})([a-f\\d]{2})([a-f\\d]{2})$"
 
-removeHashPrefix : String -> String
+removeHashPrefix : Hex -> Hex
 removeHashPrefix str =
   String.replace "#" "" str
 
 -- TODO: should this have validation on string length?
 -- TODO: should this be better lol
-shortHexToFullHex : String -> String
+shortHexToFullHex : Hex -> Hex
 shortHexToFullHex value =
   case String.length value of
     3 -> List.foldl (\v s -> s ++ v ++ v) "" (String.split "" value)
     _ -> value
 
-parseHexString : String -> Int
-parseHexString hexVal =
+parseIntHex : Hex -> Float
+parseIntHex hexVal =
   case ParseInt.parseIntHex hexVal of
-    Ok int -> int
+    Ok int -> toFloat int
     _ -> 0
 
 -- TODO: fix parseHexAsRgb behaviour - just putting bad stuff here rn
-parseHexAsRgb : String -> List Int
+parseHexAsRgb : Hex -> Rgb
 parseHexAsRgb hex =
   case List.head (Regex.find hexRegex hex) of
     Just match ->
       case .submatches match of
-        Just(a) :: Just(b) :: Just(c) :: _ -> List.map parseHexString [a, b, c]
-        _ -> [0, 0, 0]
-    Nothing -> [0, 0, 0]
+        Just(r) :: Just(g) :: Just(b) :: _ ->
+          { r = parseIntHex(r), g = parseIntHex(g), b = parseIntHex(b) }
+        _ ->
+          { r = 0, g = 0, b = 0 }
+    Nothing ->
+      { r = 0, g = 0, b = 0 }
 
-hexToRgb : String -> String
-hexToRgb hex =
-  String.concat
-  [ "rgb("
-  , String.join ", " (List.map String.fromInt (hex |> removeHashPrefix |> shortHexToFullHex |> parseHexAsRgb))
-  , ")"
-  ]
+normaliseHue : Float -> Float
+normaliseHue hue =
+  if hue < 0 then
+    toFloat (round hue + 360)
+  else
+    toFloat (round hue)
+
+-- TODO: do better
+rgbToHsl : Rgb -> Hsl
+rgbToHsl rgb =
+  let
+    (r, g, b) = (rgb.r / 255, rgb.g / 255, rgb.b / 255)
+    min = (Maybe.withDefault 0 (List.minimum [r, g, b]))
+    max = (Maybe.withDefault 0 (List.maximum [r, g, b]))
+
+    delta = max - min
+    hueDecimal =
+      if max == min then
+        0
+      else if max == r then
+        (g - b) / delta
+      else if max == g then
+        2 + (b - r) / delta
+      else
+        4 + (r - g) / delta
+
+    hue = (Maybe.withDefault 0 (List.minimum [hueDecimal * 60, 360]))
+
+    lightness = (min + max) / 2
+
+    saturation =
+      if max == min then
+        0
+      else if lightness <= 0.5 then
+        delta / (min + max)
+      else
+        delta / (2 - min - max)
+  in
+    { h = normaliseHue hue
+    , s = toFloat (round (saturation * 100))
+    , l = toFloat (round (lightness * 100))
+    }
+
+rgbToString : Rgb -> String
+rgbToString rgb =
+  "rgb(" ++
+  (String.fromFloat rgb.r) ++
+  ", " ++
+  (String.fromFloat rgb.g) ++
+  ", " ++
+  (String.fromFloat rgb.b) ++
+  ")"
+
+hslToString : Hsl -> String
+hslToString hsl =
+  "hsl(" ++
+  (String.fromFloat hsl.h) ++
+  ", " ++
+  (String.fromFloat hsl.s) ++
+  "%, " ++
+  (String.fromFloat hsl.l) ++
+  "%)"
+
+hexToRgbString : Hex -> String
+hexToRgbString hex =
+  hex
+  |> removeHashPrefix
+  |> shortHexToFullHex
+  |> parseHexAsRgb
+  |> rgbToString
+
+hexToHslString : Hex -> String
+hexToHslString hex =
+  hex
+  |> removeHashPrefix
+  |> shortHexToFullHex
+  |> parseHexAsRgb
+  |> rgbToHsl
+  |> hslToString
--- tests/ColourTests.elm
+++ tests/ColourTests.elm
@@ -4,37 +4,64 @@ import Test exposing (..)
 import Expect
 import Colour
 
-example : String -> String -> Test
-example input expected =
+example : (String -> String) -> String -> String -> Test
+example fn input expected =
   test (String.concat [input, " = ", expected]) <|
     \_ ->
-      Colour.hexToRgb input
+      fn input
         |> Expect.equal expected
 
 suite : Test
 suite =
   describe "Colour"
-  [ describe "hexToRgb"
+  [ describe "hexToRgbString"
     [ describe "hex values can be converted" <|
-        [ example "FFFFFF" "rgb(255, 255, 255)"
-        , example "EEEEEE" "rgb(238, 238, 238)"
-        , example "000000" "rgb(0, 0, 0)"
+        [ example (Colour.hexToRgbString) "FFFFFF" "rgb(255, 255, 255)"
+        , example (Colour.hexToRgbString) "EEEEEE" "rgb(238, 238, 238)"
+        , example (Colour.hexToRgbString) "000000" "rgb(0, 0, 0)"
         ]
     , describe "lowercase hex values can be converted" <|
-        [ example "ffffff" "rgb(255, 255, 255)"
-        , example "eeeeee" "rgb(238, 238, 238)"
-        , example "000000" "rgb(0, 0, 0)"
+        [ example (Colour.hexToRgbString) "ffffff" "rgb(255, 255, 255)"
+        , example (Colour.hexToRgbString) "eeeeee" "rgb(238, 238, 238)"
+        , example (Colour.hexToRgbString) "000000" "rgb(0, 0, 0)"
         ]
     , describe "three character hex values can be converted" <|
-        [ example "eee" "rgb(238, 238, 238)"
-        , example "fff" "rgb(255, 255, 255)"
-        , example "EEE" "rgb(238, 238, 238)"
-        , example "FFF" "rgb(255, 255, 255)"
+        [ example (Colour.hexToRgbString) "eee" "rgb(238, 238, 238)"
+        , example (Colour.hexToRgbString) "fff" "rgb(255, 255, 255)"
+        , example (Colour.hexToRgbString) "EEE" "rgb(238, 238, 238)"
+        , example (Colour.hexToRgbString) "FFF" "rgb(255, 255, 255)"
         ]
     , describe "hex values can have a hash prefix" <|
-        [ example "#ffffff" "rgb(255, 255, 255)"
-        , example "#eee" "rgb(238, 238, 238)"
-        , example "#000000" "rgb(0, 0, 0)"
+        [ example (Colour.hexToRgbString) "#ffffff" "rgb(255, 255, 255)"
+        , example (Colour.hexToRgbString) "#eee" "rgb(238, 238, 238)"
+        , example (Colour.hexToRgbString) "#000000" "rgb(0, 0, 0)"
+        ]
+    ]
+  , describe "hexToHslString"
+    [ describe "hex values can be converted" <|
+        [ example (Colour.hexToHslString) "FFFFFF" "hsl(0, 0%, 100%)"
+        , example (Colour.hexToHslString) "EEEEEE" "hsl(0, 0%, 93%)"
+        , example (Colour.hexToHslString) "000000" "hsl(0, 0%, 0%)"
+        , example (Colour.hexToHslString) "FFE100" "hsl(53, 100%, 50%)"
+        , example (Colour.hexToHslString) "442ED1" "hsl(248, 64%, 50%)"
+        ]
+    , describe "lowercase hex values can be converted" <|
+        [ example (Colour.hexToHslString) "ffffff" "hsl(0, 0%, 100%)"
+        , example (Colour.hexToHslString) "eeeeee" "hsl(0, 0%, 93%)"
+        , example (Colour.hexToHslString) "000000" "hsl(0, 0%, 0%)"
+        ]
+    , describe "three character hex values can be converted" <|
+        [ example (Colour.hexToHslString) "eee" "hsl(0, 0%, 93%)"
+        , example (Colour.hexToHslString) "fff" "hsl(0, 0%, 100%)"
+        , example (Colour.hexToHslString) "EEE" "hsl(0, 0%, 93%)"
+        , example (Colour.hexToHslString) "FFF" "hsl(0, 0%, 100%)"
+        , example (Colour.hexToHslString) "20f" "hsl(248, 100%, 50%)"
+        ]
+    , describe "hex values can have a hash prefix" <|
+        [ example (Colour.hexToHslString) "#ffffff" "hsl(0, 0%, 100%)"
+        , example (Colour.hexToHslString) "#eee" "hsl(0, 0%, 93%)"
+        , example (Colour.hexToHslString) "#000000" "hsl(0, 0%, 0%)"
+        , example (Colour.hexToHslString) "#FFE100" "hsl(53, 100%, 50%)"
         ]
     ]
   ]
